4017a63a747dbfdbdc141b4fd92cfde3260e565a
Andy <ajmrkva@gmail.com>
2020-02-02 05:05:25 -0500

    basically have a rising tide

---
+++ Repair the Planet - Save the Future/Assets/RisingTide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisingTide : MonoBehaviour
+{
+
+    public float position_y;
+    public float increment_y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("RiseTide", 3.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate() 
+    {
+        position_y = transform.position.y;   
+    }
+
+    void RiseTide() {
+        transform.position = new Vector3(transform.position.x, transform.position.y + increment_y, transform.position.z);
+    }
+}
