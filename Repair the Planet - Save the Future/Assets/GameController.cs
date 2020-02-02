143c0f24a650892d53f47f44a52f87dda6574d06
Andy <ajmrkva@gmail.com>
2020-02-02 09:48:09 -0500

    Basically have things but no enemies

---
+++ Repair the Planet - Save the Future/Assets/GameController.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float gamestate;
+    public float sealevelmax;
+    public float currentsealevel;
+    public Transform sealeveltransform;
+
+    public float victories;
+    public float victorygoal;
+
+    public float victorystatus;
+
+    public GameObject sky;
+    public GameObject water;
+    
+    public GameObject[] tenpercent;
+    public GameObject[] twentypercent;
+    public GameObject[] thirtypercent;
+    public GameObject[] fourtypercent;
+    public GameObject[] fiftypercent;
+    public GameObject[] sixtypercent;
+    public GameObject[] seventypercent;
+    public GameObject[] eightypercent;
+    public GameObject[] ninetypercent;
+    public GameObject[] onehundredpercent;
+
+    void Start()
+    {
+        gamestate = 1; // Game has started
+
+        InvokeRepeating("UpdateSeaLevel", 1.0f, 1.0f);
+
+        InvokeRepeating("Survive", 1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentsealevel>sealevelmax)
+            Debug.Log("Game Over");
+
+        if (victories>0)
+            victorystatus = victories/victorygoal * 100;
+
+
+        if (victorystatus>99)
+        {
+            foreach (var obj in onehundredpercent) {
+                obj.SetActive(true);
+            }
+            gamestate = 2;
+        }
+        else if (victorystatus>90)
+        {
+            foreach (var obj in ninetypercent) {
+                obj.SetActive(true);
+            }
+        }
+        else if (victorystatus>80)
+        {
+            foreach (var obj in eightypercent) {
+                obj.SetActive(true);
+            }
+        }
+        else if (victorystatus>70)
+        {
+            foreach (var obj in seventypercent) {
+                obj.SetActive(true);
+            }
+        }
+        else if (victorystatus>60)
+        {
+            foreach (var obj in sixtypercent) {
+                obj.SetActive(true);
+            }
+        }
+        else if (victorystatus>50)
+        {
+            foreach (var obj in fiftypercent) {
+                obj.SetActive(true);
+            }
+        }
+        else if (victorystatus>40)
+        {
+            foreach (var obj in fourtypercent) {
+                obj.SetActive(true);
+            }
+        }
+        else if (victorystatus>30)
+        {
+            foreach (var obj in thirtypercent) {
+                obj.SetActive(true);
+            }
+        }
+        else if (victorystatus>20)
+        {
+            foreach (var obj in twentypercent) {
+                obj.SetActive(true);
+            }
+        }
+        else if (victorystatus>10) 
+        {   
+            foreach (var obj in tenpercent) {
+                obj.SetActive(true);
+            }
+        }
+    }
+
+    public float GetVictoryStatus()
+    {
+        return(victorystatus);
+    }
+ 
+    private void UpdateSeaLevel()
+    {
+        if (gamestate==1)
+            currentsealevel = sealeveltransform.position.y;
+        
+    }
+
+    private void Survive()
+    {
+        victories += 1;
+    }
+}
---
+++ Repair the Planet - Save the Future/Assets/TreeTransition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeTransition : MonoBehaviour
+{
+
+    public GameObject gamecon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamecon = GameObject.Find("MainHeroCharacter");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.activeSelf)
+        {
+            float vs = gamecon.GetComponent<GameController>().GetVictoryStatus();
+            transform.localScale += new Vector3(vs/100,vs/100,vs/100);
+        }
+            
+    }
+}
