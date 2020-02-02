082c3a9a6b1317fe918e4ef73087df47c856a6ca
Andy <ajmrkva@gmail.com>
2020-02-02 06:31:54 -0500

    Got the camera and scenery how I want it in general

---
+++ Repair the Planet - Save the Future/Assets/PlayerAnchor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnchor : MonoBehaviour
+{
+
+    public Transform tracking_x;
+    public float track_buffer = 1.75f;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame  
+    private void FixedUpdate() {
+        transform.position = new Vector3(tracking_x.position.x-track_buffer, transform.position.y, transform.position.z);
+    }
+}
